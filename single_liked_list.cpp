9211e19ac55d363e43f9281909aa0b83f79bfd6d
melkamuendriyas714-cmd <melkamuendriyas714@gmail.com>
2026-04-08 01:06:30 -0700

    The singly linked list to store the student information
    Implement functions to add student information to a singly linked list.
    The singly linked list to store the student information at the beginning, at the end, and at specific position of the node and also we can delete the entered information

---
+++ single_liked_list.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+struct StudentType
+{
+string name;
+string Id;
+string Department;
+double GPA;
+StudentType*next;
+};
+
+StudentType*Head=NULL;
+
+void Add_Information_At_Start ()
+{
+  	StudentType*temp=new StudentType;
+	cout<<"\nEnter your name";
+	cin>>temp->name;
+	cout<<"\n Enter your ID Number";
+	cin>>temp->Id;
+	cout<<"\n Enter youor Department ";
+	cin>>temp->Department;
+	cout<<"\n Enter your GPA ";
+	cin>>temp->GPA;
+if (Head==NULL)
+{
+temp->next=NULL;
+}
+else{
+temp->next=Head;
+}
+Head=temp;
+cout<<"\n The student information is successfully stord at the first node";
+}
+
+void Add_Information_At_End ()
+{
+ StudentType*temp=new StudentType;
+ StudentType*current;
+	cout<<"\nEnter your name";
+	cin>>temp->name;
+	cout<<"\n Enter your ID Number";
+	cin>>temp->Id;
+	cout<<"\n Enter youor Department ";
+	cin>>temp->Department;
+	cout<<"\n Enter your GPA ";
+	cin>>temp->GPA;
+
+temp->next=NULL //we have to assighn the temporary node to NULL beacuse it is the last node .
+  if (Head==NULL)
+{
+ Head=temp;
+}
+else 
+{
+current=Head;
+while(current->next!=NULL)
+{
+  current=current->next;
+}
+current->next=temp;
+cout<<"\n The Student information is successfully Stored at the End";
+}
+
+void Add_Information_At_Specific_Position (int pos)
+{
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
